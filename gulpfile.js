1d906708d970e542b6e6fcef834c54790e2416b5
idoub <idoub@homeadvisor.com>
2017-03-21 17:09:55 -0600

    Nice clean installation package

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,16 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var rename = require('gulp-rename');
 var minify = require('gulp-minify');
 var fs = require('fs');
 
 var package = JSON.parse(fs.readFileSync('./package.json'));
 
-var name = `jul_${package.version}`;
+var name = `jul${package.version}`;
 var dest = 'dist';
 
 gulp.task('default',function(){
     gulp.src(['core/**/*.js','modules/**/*.js'])
         .pipe(concat(`${name}.js`))
-        .pipe(gulp.dest(dest))
-        .pipe(rename(`${name}.min.js`))
         .pipe(minify())
         .pipe(gulp.dest(dest));
 });
