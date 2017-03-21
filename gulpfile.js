ab19f4c67d85a6b3eb68950500ce045452b0b9ff
idoub <idoub@homeadvisor.com>
2017-03-21 17:36:26 -0600

    Added jsdoc generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,43 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minify = require('gulp-minify');
-var fs = require('fs');
+var jsdoc = require('gulp-jsdoc3');
 
+var docConf = require('./jsdoc.json');
+
+var fs = require('fs');
 var package = JSON.parse(fs.readFileSync('./package.json'));
 
 var name = `jul${package.version}`;
 var dest = 'dist';
 
-gulp.task('default',function(){
-    gulp.src(['core/**/*.js','modules/**/*.js'])
+var modules = [
+    'core/jul.js',
+    'modules/pubsub.js',
+    'modules/ready.js',
+    'modules/exists.js',
+    'modules/extend.js',
+    'modules/each.js',
+    'modules/map.js',
+    'modules/filter.js',
+    'modules/ajax.js',
+    'modules/addClass.js'
+];
+
+gulp.task('dist',function(){
+    gulp.src(modules)
         .pipe(concat(`${name}.js`))
         .pipe(minify())
         .pipe(gulp.dest(dest));
 });
+
+gulp.task('doc',function(){
+    gulp.src(['core/**/*.js','modules/**/*.js'], {read: false})
+        .pipe(jsdoc(docConf));
+});
+
+gulp.task('watch',function(){
+    gulp.watch('./**/*.js',['dist','doc']);
+});
+
+gulp.task('default',['dist','doc']);
