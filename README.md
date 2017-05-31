1ae7e6208c5feca138286aea46f2707fe637b686
idoub <idoub@homeadvisor.com>
2017-05-31 11:59:08 -0600

    Improve README and simplify gulpfile by moving modules to json

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
-JUL
-===
+# JUL
 
-A nifty little **J**avascript **U**tility **L**ibrary
-------------------------------------------------------------
+> A nifty little **J**avascript **U**tility **L**ibrary
 
-# Features
+## Table of Contents
+- [Features](#features)
+- [Background](#background)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Documentation](#documentation)
+- [Contribute](#contribute)
+- [License](#license)
+
+## Features
 * **Modular** - Each piece of functionality is defined in it's own module and can be included as you please.
 * **Tiny** - At under 5kb (Minified and GZipped) it weighs in at less than a sixth of jQuery's size. And that's with _all_ the modules included!
-* **Powerful** - 
+* **Powerful** - Includes the most useful features of other libraries without sacrificing size.
+* **Compatible** - Works with IE9 and up as well as all modern browsers.
 
-# Installation
-This repository only contains the source code for the library, no distribution files. If you want to use the library, simply clone and run `npm install`. Concatenated and minified files will be placed in a new **dist** folder and documentation will be generated in a **docs** folder.
+## Background
+
+
+## Installation
+This repository only contains the source code for the library, no distribution files. This is intended to prevent people from including the entire library for projects where they only need a few of the libraries features.
+
+If you want to use the library, simply clone, modify `gulpfile.js` to include the modules that you need, and run `npm install`. Concatenated and minified files will be placed in a new **dist** folder and documentation will be generated in a **docs** folder.
+
+## Usage
+
+## Documentation
+
+## Contribute
+
+## License
+[MIT © 2017 Isaac Doub](LICENSE)
--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,34 @@
 /* jshint esversion: 6 */
-const gulp = require(`gulp`);
-const concat = require(`gulp-concat`);
-const minify = require(`gulp-minify`);
-const jsdoc = require(`gulp-jsdoc3`);
-const sizereport = require(`gulp-sizereport`);
-const phantom = require(`gulp-mocha-phantomJS`);
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
+const jsdoc = require('gulp-jsdoc3');
+const sizereport = require('gulp-sizereport');
+const phantom = require('gulp-mocha-phantomJS');
+const docConf = require('./jsdoc.json');
+const package = require('./package.json');
+const modules = require('./modules.json');
 
-const docConf = require(`./jsdoc.json`);
-
-const fs = require(`fs`);
-const package = JSON.parse(fs.readFileSync(`./package.json`));
-
-const name = `jul${package.version}`;
-const modules = [
-  `src/jul.js`,
-  `src/addModule.js`,
-  `src/add.js`,
-  `src/create.js`,
-  `src/each.js`,
-  `src/exists.js`,
-  `src/extend.js`,
-  `src/filter.js`,
-  `src/hash.js`,
-  `src/map.js`,
-  `src/pubsub.js`,
-  `src/ready.js`,
-  `src/addClass.js`,
-  `src/after.js`,
-  `src/ajax.js`,
-  `src/append.js`,
-  `src/attrs.js`
-];
-
-gulp.task(`dist`, function () {
+gulp.task('dist', function () {
   return gulp.src(modules)
-    .pipe(concat(`${name}.js`))
+    .pipe(concat(`jul${package.version}.js`))
     .pipe(minify())
     .pipe(sizereport({ gzip: true, '*': { 'maxMinifiedGzippedSize': 5000 } }))
-    .pipe(gulp.dest(`dist`));
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task(`doc`, [`dist`], function () {
-  return gulp.src([`dist/${name}.js`,`README.md`], { read: false })
+gulp.task('doc', ['dist'], function () {
+  return gulp.src([`dist/jul${package.version}.js`,'README.md'], { read: false })
     .pipe(jsdoc(docConf));
 });
 
-gulp.task(`test`, [`dist`], function () {
-  return gulp.src(`./test/index.html`, { read: false })
-    .pipe(phantom({reporter: `dot`}));
+gulp.task('test', ['dist'], function () {
+  return gulp.src('./test/index.html', { read: false })
+    .pipe(phantom({reporter: 'dot'}));
 });
 
-gulp.task(`watch`, function () {
-  gulp.watch(`./**/*.js`, [`dist`, `doc`, `test`]);
+gulp.task('watch', function () {
+  gulp.watch('./**/*.js', ['dist', 'doc', 'test']);
 });
 
-gulp.task(`default`, [`dist`, `doc`, `test`]);
+gulp.task('default', ['dist', 'doc', 'test']);
