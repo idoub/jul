7ea994cc9cc090452b37e53cdb2e4bce520c6b04
idoub <idoub@homeadvisor.com>
2017-06-09 12:08:51 -0600

    One way binding, data to DOM, working

--- test/bind.js
+++ test/bind.js
@@ -1,38 +1,86 @@
 (function (_) {
   'use strict';
   _.addModule('bind', function () {
-    _.bound = _.bound || {};
+    var Gem = function() {
+      var self;
+      var bound = [];
 
-    var findDescendant = function(generations) {
-      var child = generations.shift();
-      _.bound[child] = _.bound[child] || {};
-      if(generation.length === 0) return child;
-      return findDescendant(generations);
+      this.addChild = function(childName) {
+        self = self || {};
+        if(self.hasOwnProperty(childName)) return this;
+        self[childName] = undefined;
+        Object.defineProperty(this,childName, {
+          enumerable: true,
+          get: function() {
+            return self[childName];
+          },
+          set: function(newValue) {
+            self[childName] = newValue;
+            _(bound).each(function(el){
+              updateElement(childName,el,newValue);
+            });
+          }
+        });
+
+        return this;
+      };
+
+      this.bind = function(element) {
+        if(bound.indexOf(element) > -1) return;
+        bound.push(element);
+      };
     };
 
-    var bind = function(e) {
-      if(!(e instanceof Node)) {
-        console.log(e,' is not recognised as a Node and will not be bound.');
+    this.Gem = Gem;
+
+    this.bound = this.bound || new Gem();
+
+    var updateElement = function(prop,element,value) {
+      switch(prop) {
+        case 'text':
+          element.innerText = value;
+          break;
+        default:
+          element.setAttribute(prop,value);
+          break;
+      }
+    };
+
+    var findDescendant = function(generations,parent) {
+      parent = parent || _.bound;
+      if(!(parent instanceof Gem)) parent = new Gem();
+      var childName = generations.shift();
+      parent.addChild(childName);
+      parent[childName] = parent[childName] || new Gem();
+      if(generations.length === 0) return parent[childName];
+      return findDescendant(generations,parent[childName]);
+    };
+
+    var elementChange = function(evt) {
+      console.log(evt.target, 'changed');
+    };
+
+    var bind = function(element) {
+      if(!(element instanceof Node)) {
+        console.log(element,' is not recognised as a Node and will not be bound.');
         return;
       }
 
-      var data = _(e).data().j;
+      var data = _(element).data().j;
       if(typeof data === 'undefined') {
-        console.log(e,' does not have the binding attribute \'data-j\' and will not be bound.');
+        console.log(element,' does not have the binding attribute \'data-j\' and will not be bound.');
       }
 
-      // Sanitize data-j attribute
+      element.addEventListener('change',elementChange);
+      element.addEventListener('input',elementChange);
+
       data = data.replace(/\r?\n|\r|\s/g,'');
-      var attributes = data.split(',');
-      for(var i=0;i<attributes.length;i++) {
-        var segments = attributes[i].split(':');
-        var content = findDescendant(segments[1].split('.'));
-        if(segments[0] === 'text') {
-          e.innerText = content;
-        } else {
-          e.setAttribute(segments[0],content);
-        }
+      var bindings = data.split(':');
+      var boundObj = findDescendant(bindings.shift().split('.'));
+      for(var i=0;i<bindings.length;i++) {
+        boundObj.addChild(bindings[i]);
       }
+      boundObj.bind(element);
     };
 
     this.bind = bind;
@@ -41,5 +89,5 @@
         bind(e);
       });
     };
-  },['each','data','extend']);
+  },['each','data','extend','pubsub']);
 })(_||{});
